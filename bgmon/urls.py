10636e9ec62fe86e0a9ec3c22cc1c14259a472e3
Ken Iikura <keniikura@gmail.com>
2015-04-11 01:38:39 +0900

    デザイン中

--- bgmon/urls.py
+++ bgmon/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'bgmon.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^bgdash/', include('bgdash.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
