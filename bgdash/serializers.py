970e1671fa62e56fe5ab7be5226bffe8be05dc00
Ken Iikura <ken.iikura.xe@hitachi-systems.com>
2015-05-08 20:07:55 +0900

    idsシリアライザを使ってみる

--- bgdash/serializers.py
+++ bgdash/serializers.py
@@ -4,6 +4,40 @@ from rest_framework import serializers
 import models
 
 
+class LaneIdsSerializer(serializers.Serializer):
+    dest_lanes = serializers.ListField(
+                    child=serializers.IntegerField()
+                    )
+    
+    
+    def __init__(self, site=None, **kwargs):
+        super(LaneIdsSerializer, self).__init__(**kwargs)
+        self.site = site
+        self.dest = []
+        self.src = []
+        
+    def validate(self, data):
+        """
+        Check lanid exists.
+        """
+        db_lanes = models.Lane.objects.filter(site=self.site)
+        laneids = list(data['dest_lanes'])
+        dest = []
+        src = []
+        for l in db_lanes:
+            if l.id in laneids:
+                dest.append(l)
+                laneids.remove(l.id)
+            else:
+                src = l.id
+        
+        
+        if  len(laneids) > 0:
+            raise serializers.ValidationError("invalid dest_lanes id for current site. ids=%s" % (",".join(map(str, laneids))))
+        
+        self.dest = dest
+        self.src = src
+        return data
 
 class LaneSerializer(serializers.ModelSerializer):
     active = serializers.ReadOnlyField()
--- bgdash/views.py
+++ bgdash/views.py
@@ -1,16 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+
 from django.contrib.auth.models import User, Group
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+
 from bgdash.serializers import (
     ListSiteSerializer,
     SiteSerializer,
     LaneSerializer,
     UserSerializer,
     GroupSerializer,
+    LaneIdsSerializer
     )
 from bgdash import models
 
-import subprocess
+
 
 
 
@@ -24,7 +33,16 @@ class SiteViewSet(viewsets.ModelViewSet):
         else:
             return ListSiteSerializer
 
-
+    @detail_route(methods=['post'])
+    def switch(self, request, pk=None):
+        site = self.get_object()
+        serializer = LaneIdsSerializer(data=dict(request.data),site=site)
+        if serializer.is_valid():
+            print serializer.dest
+            return Response({'status': serializer.data})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
 
 class UserViewSet(viewsets.ModelViewSet):
     """
